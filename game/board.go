74275b4f359891f0cbd7b09abbfc558e18670b77
Oluwafemi Sule <oluwafemisule@outlook.com>
2018-10-11 14:36:02 +0200

    Implement Board data object The following methods are implemented for the Board         String()         CheckGame()         PlaceMark()

--- game/board.go
+++ game/board.go
@@ -8,20 +8,75 @@ type Board struct {
 	grid [][]string
 }
 
+const GRID_SIZE int = 4
+
 // Create a board
-func (Board) Board() {
+func NewBoard() Board {
+	g := make([][]string, GRID_SIZE)
+	b := Board{grid: g}
+	for i := 0; i < GRID_SIZE; i++ {
+		b.grid[i] = make([]string, GRID_SIZE)
+		for j := 0; j < GRID_SIZE; j++ {
+			b.grid[i][j] = "-"
+		}
+	}
+	return b
+}
+
+func (b *Board) matchAlongRow(mark string) bool {
+	for i := 0; i < GRID_SIZE; i++ {
+		matchFound := true
+		for j := 0; j < GRID_SIZE; j++ {
+			matchFound = matchFound && b.grid[i][j] == mark
+		}
+		if matchFound {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *Board) matchAlongDiagonal(mark string) bool {
+	matchFound := true
+	for i := 0; i < GRID_SIZE; i++ {
+		matchFound = matchFound && b.grid[i][i] == mark
+	}
+	if matchFound {
+		return true
+	}
+	return false
+}
+
+func (b *Board) matchAlongColumn(mark string) bool {
+	for i := 0; i < GRID_SIZE; i++ {
+		matchFound := true
+		for j := 0; j < GRID_SIZE; j++ {
+			matchFound = matchFound && b.grid[j][i] == mark
+		}
+		if matchFound {
+			return true
+		}
+	}
+	return false
 }
 
 // Check if there are matching cells in a direction
-func (Board) CheckGame(mark string) bool {
-	return true
+func (b *Board) CheckGame(mark string) bool {
+	return b.matchAlongRow(mark) || b.matchAlongDiagonal(mark) || b.matchAlongColumn(mark)
 }
 
 // Place mark in location on board
-func (Board) PlaceMark(row int, column int, mark string) {
+func (b *Board) PlaceMark(row int, column int, mark string) {
+	b.grid[row][column] = mark
 }
 
 // Implement Stringer interface
-func (Board) String() string {
-	return strings.Repeat("- - - -\n", 4)
+func (b Board) String() string {
+	builder := strings.Builder{}
+
+	for i := 0; i < GRID_SIZE; i++ {
+		builder.WriteString(strings.Join(b.grid[i], " "))
+		builder.Write([]byte("\n"))
+	}
+	return builder.String()
 }
